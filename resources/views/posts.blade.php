bab3855b89becdd86da5b397472adfbe659ab3e2
Prithvi Tambe <prithvitambe2@gmail.com>
2021-11-05 14:33:04 +0530

    Dynamic content loading
    Now the posts are loaded dynamically

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="post">My First Post</a></h1>
+        <h1><a href="posts/my-first-post">My First Post</a></h1>
 
         <p>
             Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut rutrum nisl, vel finibus enim. Nulla eget tincidunt tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur congue ipsum turpis, id porttitor elit consequat non. Nullam euismod volutpat sem ut facilisis. Mauris quis est id quam sagittis consectetur. Praesent sem sem, consequat pretium sollicitudin vel, lobortis sit amet sem. Morbi urna dolor, sagittis a erat sit amet, lobortis rhoncus eros. Donec malesuada rhoncus nibh vel rhoncus. Duis et vehicula ligula, a molestie augue. Praesent quis metus leo. Aliquam a dolor vitae elit dapibus dictum non at quam. Phasellus pharetra rhoncus risus vitae pulvinar. Aenean imperdiet purus sed felis varius convallis. Praesent mattis ex non tempor molestie. Quisque eu tristique turpis, bibendum feugiat augue. Vestibulum gravida cursus imperdiet. In diam felis, rutrum id laoreet at, laoreet nec turpis. Nunc imperdiet placerat nunc varius imperdiet. Morbi nulla nisl, tincidunt vitae eros ac, luctus fermentum nisi. Nullam a porta lectus.
@@ -13,7 +13,7 @@
     </article>
 
     <article>
-        <h1><a href="post">My Second Post</a></h1>
+        <h1><a href="posts/my-second-post">My Second Post</a></h1>
 
         <p>
             Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut rutrum nisl, vel finibus enim. Nulla eget tincidunt tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur congue ipsum turpis, id porttitor elit consequat non. Nullam euismod volutpat sem ut facilisis. Mauris quis est id quam sagittis consectetur. Praesent sem sem, consequat pretium sollicitudin vel, lobortis sit amet sem. Morbi urna dolor, sagittis a erat sit amet, lobortis rhoncus eros. Donec malesuada rhoncus nibh vel rhoncus. Duis et vehicula ligula, a molestie augue. Praesent quis metus leo. Aliquam a dolor vitae elit dapibus dictum non at quam. Phasellus pharetra rhoncus risus vitae pulvinar. Aenean imperdiet purus sed felis varius convallis. Praesent mattis ex non tempor molestie. Quisque eu tristique turpis, bibendum feugiat augue. Vestibulum gravida cursus imperdiet. In diam felis, rutrum id laoreet at, laoreet nec turpis. Nunc imperdiet placerat nunc varius imperdiet. Morbi nulla nisl, tincidunt vitae eros ac, luctus fermentum nisi. Nullam a porta lectus.
@@ -21,7 +21,7 @@
     </article>
 
     <article>
-        <h1><a href="post">My Third Post</a></h1>
+        <h1><a href="posts/my-third-post">My Third Post</a></h1>
 
         <p>
             Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut rutrum nisl, vel finibus enim. Nulla eget tincidunt tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur congue ipsum turpis, id porttitor elit consequat non. Nullam euismod volutpat sem ut facilisis. Mauris quis est id quam sagittis consectetur. Praesent sem sem, consequat pretium sollicitudin vel, lobortis sit amet sem. Morbi urna dolor, sagittis a erat sit amet, lobortis rhoncus eros. Donec malesuada rhoncus nibh vel rhoncus. Duis et vehicula ligula, a molestie augue. Praesent quis metus leo. Aliquam a dolor vitae elit dapibus dictum non at quam. Phasellus pharetra rhoncus risus vitae pulvinar. Aenean imperdiet purus sed felis varius convallis. Praesent mattis ex non tempor molestie. Quisque eu tristique turpis, bibendum feugiat augue. Vestibulum gravida cursus imperdiet. In diam felis, rutrum id laoreet at, laoreet nec turpis. Nunc imperdiet placerat nunc varius imperdiet. Morbi nulla nisl, tincidunt vitae eros ac, luctus fermentum nisi. Nullam a porta lectus.
